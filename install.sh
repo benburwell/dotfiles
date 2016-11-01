bab59930627514d3eb518dedaee02a7ea757f334
Ben Burwell <ben.burwell@trifecta.com>
2016-10-31 22:33:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,17 +9,36 @@ done
 for f in $(find . -type f); do
 	src="$HOME/.dotfiles/links/$f"
 	dst="$HOME/$f"
-	ln -s $src $dst
+	if [ ! -f $dst ]; then
+		echo "===> Symlinking $dst"
+		ln -s $src $dst
+	else
+		echo "---> Found existing $dst, skipping"
+	fi
 done
 cd $HOME
 
 # initialize vundle
 mkdir -p $HOME/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+	echo "===> Getting Vundle"
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+else
+	echo "---> Looks like we already have Vundle, skipping"
+fi
 vim +PluginInstall +qall
 
 # setup nvim
 mkdir -p $HOME/.config
-ln -s $HOME/.vim $HOME/.config/nvim
-ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
-
+if [ ! -d $HOME/.config/nvim ]; then
+	echo "===> Linking .vim for Neovim"
+	ln -s $HOME/.vim $HOME/.config/nvim
+else
+	echo "---> Looks like .vim has already been set up for Neovim"
+fi
+if [ ! -f $HOME/.config/nvim/init.vim ]; then
+	echo "===> Linking .vimrc for Neovim"
+	ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
+else
+	echo "---> Looks like .vimrc has already been set up for Neovim"
+fi
