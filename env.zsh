50b65d2231c56ee30737413cca5bba9b4bcb17d0
Ben Burwell <ben.burwell@trifecta.com>
2017-03-02 21:57:35 -0500

    Update lynx config

--- env.zsh
+++ env.zsh
@@ -4,4 +4,5 @@ export CLICOLOR=true
 export EDITOR=nvim
 export LICENSE_FULL_NAME="Ben Burwell"
 export WWW_HOME="https://www.google.com/"
-
+export LYNX_LSS="$HOME/.lynx.lss"
+export LYNX_CFG="$HOME/.lynx.cfg"
