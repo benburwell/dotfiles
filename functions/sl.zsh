1763bc47c361f4320f0350685285af43764c0f5e
Ben Burwell <ben@benburwell.com>
2017-05-17 10:38:19 -0400

    zsh: Use a scratch buffer for `sl` output

--- functions/sl.zsh
+++ functions/sl.zsh
@@ -3,8 +3,8 @@ function sl() {
 		echo "Usage: sl <log id> [ <org name ]" && return 1
 	fi
 	if [[ $# -lt 2 ]]; then
-		sf log $1 | nvim -c "set syntax=apexlog" -
+		sf log $1 | nvim -c "set syntax=apexlog" -c "set buftype=nofile" -
 		return 0
 	fi
-	sf log $1 | nvim -c "set syntax=apexlog" -
+	sf log $1 | nvim -c "set syntax=apexlog" -c "set buftype=nofile" -
 }
