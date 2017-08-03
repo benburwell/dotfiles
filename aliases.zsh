2153b75944b5ac8c8c2eebf64462cae5ca363cec
Ben Burwell <ben@benburwell.com>
2017-08-03 08:14:17 -0400

    zsh: Sort aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,71 +4,72 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 if [[ $(uname) == "Darwin" ]]; then
-  alias ls="ls -Gh"
   alias l="ls -lAhG"
-  alias ll="ls -lGh"
   alias la="ls -lAGh"
+  alias ll="ls -lGh"
+  alias ls="ls -Gh"
 fi
 if [[ $(uname) == "FreeBSD" ]]; then
-  alias ls="ls -Gh"
   alias l="ls -lAhG"
-  alias ll="ls -lGh"
   alias la="ls -lAGh"
+  alias ll="ls -lGh"
+  alias ls="ls -Gh"
 fi
 if [[ $(uname -a | grep Ubuntu) ]]; then
-  alias ls="ls --color"
   alias l="ls -lah --color"
-  alias ll="ls -l --color"
   alias la="ls -la --color"
+  alias ll="ls -l --color"
+  alias ls="ls --color"
 fi
 
 # git
 alias g="git"
-alias gl='git pull'
-alias glr='git pull --rebase'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%aN>%Creset' --abbrev-commit"
-alias gp='git push'
-alias gd='git diff'
 alias ga='git add'
-alias gc='git commit'
-alias gco='git checkout'
-alias gcb='git checkout -b'
-alias gcp='git cherry-pick'
 alias gb='git branch'
 alias gbs='git-branches'
-alias gs='git status'
-alias gr='git remote'
+alias gc='git commit'
+alias gcb='git checkout -b'
+alias gco='git checkout'
+alias gcp='git cherry-pick'
+alias gd='git diff'
+alias gf='git fetch'
+alias gl='git pull'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%aN>%Creset' --abbrev-commit"
+alias glr='git pull --rebase'
 alias gm='git merge'
+alias gp='git push'
+alias gr='git remote'
+alias gs='git status'
 alias gsl='git stash list'
 alias gsp='git stash pop'
 alias gss='git stash save'
-alias gf='git fetch'
 
 # golang
-alias goi="go install"
 alias gob="go build"
-alias gog="go get"
 alias gof="go fmt"
+alias gog="go get"
+alias goi="go install"
 
 # heroku
 alias h="heroku"
+alias hlt="heroku logs --tail"
 alias hpg="heroku pg:psql"
 alias hps="heroku ps"
 alias hpss="heroku ps:scale"
-alias hlt="heroku logs --tail"
 
 # party parrot
-alias parrot="terminal-parrot"
 alias celebrate="terminal-parrot -delay 50 -loops 3"
+alias parrot="terminal-parrot"
 
 # docker-compose
 alias dc="docker-compose"
-alias dcu="docker-compose up"
 alias dcd="docker-compose down"
 alias dcl="docker-compose pull"
+alias dcu="docker-compose up"
+alias dcud="docker-compose up -d"
 
 # docker
 alias d="docker"
+alias dim="docker images"
 alias dps="docker ps"
 alias dpsa="docker ps -a"
-alias dim="docker images"
