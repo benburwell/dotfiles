1129d15aa4393ffaf2417c028cab9c954b44d92e
Ben Burwell <ben@benburwell.com>
2017-08-03 08:14:54 -0400

    zsh: Add python/pip to $PATH

--- path.zsh
+++ path.zsh
@@ -1,2 +1,2 @@
-export PATH="$PROJECTS/bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/go/bin:$HOME/.rvm/bin:$PATH"
+export PATH="$PROJECTS/bin:$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/go/bin:$HOME/.rvm/bin:$PATH:$HOME/Library/Python/2.7/bin"
 
