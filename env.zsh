0049662c644b966bd3dca73ab4b6c885781a9b91
Ben Burwell <ben@benburwell.com>
2017-10-03 19:47:48 -0400

    env: Change WWW home to Hacker News

--- env.zsh
+++ env.zsh
@@ -3,6 +3,6 @@ export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 export EDITOR=nvim
 export LICENSE_FULL_NAME="Ben Burwell"
-export WWW_HOME="https://www.google.com/"
+export WWW_HOME="https://news.ycombinator.com/"
 export LYNX_LSS="$HOME/.lynx.lss"
 export LYNX_CFG="$HOME/.lynx.cfg"
