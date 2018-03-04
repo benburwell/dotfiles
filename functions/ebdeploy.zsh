b6ebedfadbe72bae1368feed4523a989d17af9cb
Ben Burwell <ben@benburwell.com>
2018-03-04 12:26:23 -0500

    Move functions to bins

--- functions/ebdeploy.zsh
+++
@@ -1,3 +0,0 @@
-function ebdeploy() {
-	eb deploy --timeout 30 --label "$(git rev-parse --short HEAD)-$(date +%Y-%m-%dT%H:%M:%SZ)"
-}
--- functions/ec2.zsh
+++
@@ -1,5 +0,0 @@
-function ec2() {
-	if [ $# == 0 ]; then
-		aws ec2 describe-instances | jq --raw-output 'include "aws"; getInstanceMetadata' | column -t -s $'\t' | sort
-	fi
-}
