9268a398ccbf2b82c85e144ab62e19d63ce6ad16
Ben Burwell <ben@benburwell.com>
2018-03-04 11:55:20 -0500

    Use $BROWSER to open PRs

--- env.zsh
+++ env.zsh
@@ -8,3 +8,4 @@ export LYNX_LSS="$HOME/.lynx.lss"
 export LYNX_CFG="$HOME/.lynx.cfg"
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export BROWSER="Firefox"
--- functions/rp.zsh
+++ functions/rp.zsh
@@ -6,5 +6,5 @@ function rp() {
 	local upstream=$(git config --get remote.origin.url | cut -d':' -f2 | sed 's/\.git$//')
 	local branch=$(git rev-parse --abbrev-ref HEAD)
 	local url=https://github.com/$upstream/compare/$branch?expand=1
-	open -a "Google Chrome" "$url"
+	open -a "$BROWSER" "$url"
 }
