58578c8124946c4d2c6705a6425e27b375e872bb
Ben Burwell <ben@benburwell.com>
2018-03-04 12:08:18 -0500

    Update zsh config, include FZF

--- config.zsh
+++ config.zsh
@@ -2,24 +2,39 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt APPEND_HISTORY # adds history
+setopt APPEND_HISTORY
 setopt AUTO_CD
 setopt COMPLETE_IN_WORD
 setopt CORRECT
 setopt EXTENDED_GLOB
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt EXTENDED_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
 setopt HIST_VERIFY
-setopt INC_APPEND_HISTORY # adds history incrementally and share it across sessions
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt NO_BG_NICE # don't nice background tasks
+setopt INC_APPEND_HISTORY
+setopt LOCAL_OPTIONS
+setopt LOCAL_TRAPS
+setopt NO_BG_NICE
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt PROMPT_SUBST
 
-zle -N newtab
+# open a file for editing by choosing with fzf
+fzf-open-file-or-dir() {
+	local cmd="command find -L . \
+	\\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
+	-o -type f -print \
+	-o -type d -print \
+	-o -type l -print 2> /dev/null | sed 1d | cut -b3-"
+	local out=$(eval $cmd | fzf --exit-0)
+	if [ -f "$out" ]; then
+		$EDITOR "$out" < /dev/tty
+	elif [ -d "$out" ]; then
+		cd "$out"
+		zle reset-prompt
+	fi
+}
+zle -N fzf-open-file-or-dir
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
@@ -29,4 +44,4 @@ bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
 bindkey '^R' history-incremental-search-backward
-bindkey -s '^P' 'efzf\n'
+bindkey '^P' fzf-open-file-or-dir
