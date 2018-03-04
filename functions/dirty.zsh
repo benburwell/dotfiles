a4e8c6659c86444eb4e88c6934aab45325247be7
Ben Burwell <ben@benburwell.com>
2018-03-04 12:07:30 -0500

    Don't look only for PRs going to `master` in `dirty`

--- functions/dirty.zsh
+++ functions/dirty.zsh
@@ -8,7 +8,7 @@ function dirty() {
 		is_dirty=$(git --git-dir="$p/.git" --work-tree="$p" diff --shortstat 2> /dev/null | tail -n1)
 		branch=$(git --git-dir="$p/.git" --work-tree="$p" rev-parse --abbrev-ref HEAD 2> /dev/null)
 		if [[ $branch != "master" && $branch != "HEAD" ]]; then
-			pr=$(curl --silent "https://benburwell:$GITHUB_TOKEN@api.github.com/repos/virtyx-technologies/$repo/pulls?state=open&head=virtyx-technologies:$branch&base=master" | jq --raw-output --monochrome-output '.[0]._links.html.href')
+			pr=$(curl --silent "https://benburwell:$GITHUB_TOKEN@api.github.com/repos/virtyx-technologies/$repo/pulls?state=open&head=virtyx-technologies:$branch" | jq --raw-output --monochrome-output '.[0]._links.html.href')
 			pr_link=$([[ $pr != "null" ]] && echo "($pr)")
 			if [[ $is_dirty != "" ]]; then
 				echo "$repo: ${yellow}$branch${nc} $pr_link"
