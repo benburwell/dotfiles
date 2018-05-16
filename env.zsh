a3b8b13af7bd5c3c4ec9fb777a1a6ff90680f43e
Ben Burwell <ben@benburwell.com>
2018-05-16 10:01:35 -0400

    env: Add NVM_HOME to global env

--- env.zsh
+++ env.zsh
@@ -9,3 +9,4 @@ export LYNX_CFG="$HOME/.lynx.cfg"
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export BROWSER="Firefox"
+export NVM_DIR="$HOME/.nvm"
