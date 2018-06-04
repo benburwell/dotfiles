b734a8c858de1fdf1ac40f3c122c732b9615959d
Ben Burwell <ben@benburwell.com>
2018-06-03 23:19:34 -0400

    brew: Add homebrew init script
    This script installs homebrew and installs programs I use a whole bunch.

---
+++ brewinit.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install chromium
+brew cask install gnucash
+brew cask install gpgtools
+brew cask install mactex
+brew cask install mactex
+brew cask install transmission
+brew cask install virtualbox
+brew cask install wireshark
+brew install cmake
+brew install ctags
+brew install ctop
+brew install dep
+brew install fzf
+brew install git
+brew install go
+brew install google-java-format
+brew install gpg2
+brew install hub
+brew install irssi
+brew install jq
+brew install maven
+brew install mutt
+brew install neovim
+brew install nvm
+brew install oath-toolkit
+brew install pass
+brew install pinentry
+brew install postgresql@9.5
+brew install pwgen
+brew install python3
+brew install ripgrep
+brew install shellcheck
+brew install tmux
