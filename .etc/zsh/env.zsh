f1288ba8d52192e002d7433c9932614043498297
Ben Burwell <ben@benburwell.com>
2018-06-09 23:54:13 -0400

    env: Update order of find for CDPATH
    find on Linux doesn't like -maxdepth to come after -type for some
    reason.

--- .etc/zsh/env.zsh
+++ .etc/zsh/env.zsh
@@ -9,4 +9,4 @@ export LYNX_CFG="$HOME/.lynx.cfg"
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export NVM_DIR="$HOME/.nvm"
-export CDPATH=$(find "$PROJECTS/src" -type d -maxdepth 1 -print0 | tr '\0' ':')$HOME
+export CDPATH=$(find "$PROJECTS/src" -maxdepth 1 -type d -print0 | tr '\0' ':')$HOME
