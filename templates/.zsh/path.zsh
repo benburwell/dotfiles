ef40bdbf01136aa3f825b2bae510bda7de200d52
Ben Burwell <ben@benburwell.com>
2019-10-03 16:17:48 -0400

    zsh: set path to point to current psql

--- templates/.zsh/path.zsh
+++ templates/.zsh/path.zsh
@@ -1,6 +1,6 @@
 export PATH=/usr/local/opt/node@10/bin
 export PATH=$PATH:/usr/local/opt/python/libexec/bin
-export PATH=$PATH:/usr/local/Cellar/postgresql@9.6/9.6.14/bin
+export PATH=$PATH:/usr/local/Cellar/postgresql@9.6/9.6.15/bin
 export PATH=$PATH:/Applications/Wireshark.app/Contents/MacOS
 export PATH=$PATH:/Applications/Firefox/Contents/MacOS
 export PATH=$PATH:/Library/TeX/texbin
